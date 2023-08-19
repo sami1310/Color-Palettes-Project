ad853acd433856aa506168600e4eb39b92f2cecf
sami1310 <samia.jahin@northsouth.edu>
2023-08-19 19:25:40 +0600

    fixed the search issue

--- color_palette/palettes/urls.py
+++ color_palette/palettes/urls.py
@@ -4,7 +4,6 @@ from .views import PaletteListView, palette_list_view
 app_name = "palettes"
 
 urlpatterns = [
-    path("", palette_list_view, name="palette-list-view"),
-    path("palettes", PaletteListView.as_view(), name="palette-list"),
-    # path('palettes/<int:pk>/', PaletteDetailView.as_view(), name='palette-detail'),
+    path("list/", palette_list_view, name="palette-list-view"),
+    path("palettes/", PaletteListView.as_view(), name="palette-list"),
 ]
--- color_palette/static/js/scripts.js
+++ color_palette/static/js/scripts.js
@@ -4,8 +4,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Add search functionality
     document.getElementById('search-btn').addEventListener('click', function () {
-        const query = document.getElementById('search-input').value;
-        fetchPalettes(query);
+        let query = document.getElementById('search-input').value;
+
+        fetch(`/palettes/?search=${query}`)
+            .then(response => response.json())
+            .then(data => {
+                // Clear existing palettes
+                let palettesDiv = document.querySelector('.palettes');
+                palettesDiv.innerHTML = '';
+
+                // Populate palettes with the received data
+                data.forEach(palette => {
+                    let paletteDiv = document.createElement('div');
+                    paletteDiv.classList.add('palette');
+
+                    let paletteName = document.createElement('h3');
+                    paletteName.textContent = palette.name;
+                    paletteDiv.appendChild(paletteName);
+
+                    let colorsDiv = document.createElement('div');
+                    colorsDiv.classList.add('colors');
+                    palette.dominant_colors.forEach(color => {
+                        let colorBox = document.createElement('div');
+                        colorBox.classList.add('color-box');
+                        colorBox.style.backgroundColor = color.hex;
+                        colorsDiv.appendChild(colorBox);
+                    });
+
+                    paletteDiv.appendChild(colorsDiv);
+                    palettesDiv.appendChild(paletteDiv);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching palettes:', error);
+            });
     });
 });
 
